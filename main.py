ef14492bade22518ed370d558da6eb06b9578a81
sna1lBoy <jadenray1@proton.me>
2023-07-16 15:59:47 -0500

    added new features to main

--- main.py
+++ main.py
@@ -1,70 +1,73 @@
-from time import *
-from bots.econBot import *
 from bots.commandBot import *
 from bots.pollBot import *
 from libraries.chatPlays import *
-from libraries.autoStream import *
 from libraries.charityDonoTTS import *
+from bots.panicBot import *
+from bots.gwrbullBot import *
 
 # setting stuff up
 econBotThread = startEconBot()
 commandBotThread = startCommandBot()
 pollBotThread = startPollBot()
+panicBotThread = startPanicBot()
+gwrbotThread = startGwrbullBot()
 connectToObs()
 connectToTwitchChat()
 
-# so you dont have to restart stream
+# so you don't have to restart stream
 if isLive(yourChannelName):
     startTTS()
     startChatPlays()
     startAutoSave()
     startIdleBot()
     startInputBot()
+    startInternetPing()
 
 while True:
-    try:
-        # if streamer goes live
-        if (isLive(yourChannelName) and isLive(streamerChannelName)):
-            # shut down everything
-            if ttsOn:
-                stopTTS()
-            if chatPlaying:
-                stopChatPlays()
-            if idleBotPlaying:
-                stopIdleBot()
-            if inputBotPlaying:
-                stopInputBot()
-            if autoSaving:
-                stopAutoSave()
 
-            # end stream
-            if isLive(yourChannelName):
-                raid(yourChannelName, streamerChannelName)
-                stopStream()
+    # if streamer goes live
+    if (isLive(yourChannelName) and isLive(streamerChannelName)):
 
-        # if streamer goes offline
-        elif (not isLive(yourChannelName) and not isLive(streamerChannelName)):
-            # start stream
-            if not isLive(yourChannelName):
-                startStream()
-            if not ttsOn:
-                startTTS()
-            if not chatPlaying:
-                startChatPlays()
-            if not autoSaving:
-                startAutoSave()
-            if not idleBotPlaying:
-                startIdleBot()
-            if not inputBotPlaying:
-                startInputBot()
-    # if websockets disconnects
-    except:
-        disconnectFromTwitchChat()
-        sleep(5)
-        connectToTwitchChat()
-        startChatPlays()
+        # shut down everything
+        if ttsOn:
+            stopTTS()
+        if chatPlaying:
+            stopChatPlays()
+        if idleBotPlaying:
+            stopIdleBot()
+        if inputBotPlaying:
+            stopInputBot()
+        if autoSaving:
+            stopAutoSave()
+        if pinging:
+            stopInternetPing()
 
-# so asyncio doesn't yell at me
+        # end stream
+        if isLive(yourChannelName):
+            raid(yourChannelName, streamerChannelName)
+            stopStream()
+
+    # if streamer goes offline
+    elif (not isLive(yourChannelName) and not isLive(streamerChannelName)):
+        # start stream
+        if not isLive(yourChannelName):
+            startStream()
+        if not ttsOn:
+            startTTS()
+        if not chatPlaying:
+            startChatPlays()
+        if not autoSaving:
+            startAutoSave()
+        if not idleBotPlaying:
+            startIdleBot()
+        if not inputBotPlaying:
+            startInputBot()
+        if not pinging:
+            startInternetPing()
+
+# so asyncio doesn't yell at me :)
 econBotThread.join()
 commandBotThread.join()
 pollBotThread.join()
+panicBotThread.join()
+gwrbotThread.join()
