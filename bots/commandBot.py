270d6dc080e5b5658161253318356ff93b29c015
sna1lBoy <jadenray1@proton.me>
2023-07-16 15:54:30 -0500

    added !panicCode and !panicStream

--- bots/commandBot.py
+++ bots/commandBot.py
@@ -1,70 +1,208 @@
 # responds to basic commands in chat
-# dont fuck with this too much unless youre familiar with twitchio and how it works
+# don't fuck with this too much unless youre familiar with twitchio and how it works
 # not much documentation here because even i dont know what the fuck this object oriented programming is doing in python
 
-import threading
-import csv
-from twitchio.ext import commands
-from libraries.autoStream import *
-import requests
+# imports
 from urllib.parse import urlencode
-import os
-from datetime import datetime, timezone
+from bots.econBot import *
+import sqlite3
 
-# starts command bot
+# reading config
+config = configparser.ConfigParser()
+config.read(os.path.abspath((os.path.join(directory, "config.ini"))))
+spotifyClientID = config.get("spotify", "client id")
+spotifyClientSecret = config.get("spotify", "client secret")
+spotifyRefreshToken = config.get("spotify", "spotify refresh token")
+
+# if you don't have a refresh token
+if spotifyRefreshToken == "":
+
+    # getting code from user
+    print(f'authorize this script by going to:\n{"https://accounts.spotify.com/authorize" + "?" + urlencode({"client_id": spotifyClientID, "response_type": "code", "redirect_uri": "http://localhost:8888/callback", "scope": "user-read-currently-playing user-modify-playback-state"})}')
+    authorizationCode = input("enter the authorization code found in the redirected url after \"code=\": ")
+
+    # gets access token and refresh token using the code
+    response = requests.post("https://accounts.spotify.com/api/token",auth = (spotifyClientID, spotifyClientSecret), data = {"grant_type": "authorization_code", "code": authorizationCode, "redirect_uri": "http://localhost:8888/callback"})
+
+    # writes refresh token to info file for future use
+    if 'refresh_token' in response.json():
+        with open(os.path.abspath((os.path.join(directory, "config.ini"))), 'r') as file:
+            lines = file.readlines()
+            for i, line in enumerate(lines):
+                if line.startswith("spotify refresh token ="):
+                    lines[i] = "spotify refresh token = " + response.json()["refresh_token"] + "\n"
+                    break
+            with open(os.path.abspath((os.path.join(directory, "config.ini"))), "w") as file:
+                file.writelines(lines)
+    else:
+        print("problem getting tokens " + response.json())
+
+# starts command bot in a new thread
 def startCommandBot():
     bot = Bot()
     botThread = threading.Thread(target=bot.run)
     botThread.start()
     return botThread
 
-# bot commands class
 class Bot(commands.Bot):
 
     # sets up bot and connects to twitch
     def __init__(self):
         super().__init__(token = accessToken, prefix="!", initial_channels = [yourChannelName])
 
+    # when someone sends a message in chat
+    async def event_message(self, message):
+
+        # don't take bot messages as real messages
+        if message.echo:
+            return
+
+        # reading database
+        async with databaseLock:
+            db = sqlite3.connect(os.path.abspath((os.path.join(directory, "chatData.db"))))
+            cursor = db.cursor()
+            cursor.execute("SELECT * FROM chatters WHERE id = ?", (getBroadcasterId(message.author.name),))
+            result = cursor.fetchone()
+
+            # check if id is in database
+            if not result:
+                cursor.execute("INSERT INTO chatters (id) VALUES (?)", (getBroadcasterId(message.author.name),))
+                db.commit()
+                await self.connected_channels[0].send("[bot] welcome " + message.author.name + " to early_gang, where we play games and vibe while we wait for dougdoug to stream again. right now we are trying to beat a randomized pokemon white before dougdoug streams again. the controls are up, down, right, start, a, b, x, and y (more at !controls) and additionally the snack family may try to \"help\" you out. enjoy!")
+
+            # closing database
+            cursor.close()
+            db.close()
+        await self.handle_commands(message)
+
+    # sends list of chat plays controls
     @commands.command()
     async def controls(self, ctx: commands.Context):
-        await ctx.send("[bot] (capitilization doesnt matter) up, down, left, right, hold up, hold down, hold left, hold right, a, hold a, mash a, b, hold b, mash b, x, y, l, r, start, select, stop, wander, up wander, down wander, left wander, right wander, north, south, east, west")
+        await ctx.send("[bot] (capitalization doesnt matter) up, down, left, right, hold up, hold down, hold left, hold right, a, hold a, mash a, b, hold b, mash b, x, y, l, r, start, select, stop, wander, up wander, down wander, left wander, right wander, north, south, east, west")
 
+    # sends what's going on
     @commands.command()
     async def what(self, ctx: commands.Context):
-        await ctx.send(
-            "[bot] chat tries to beat randomised pokemon heart gold before dougdoug streams again")
+        await ctx.send("[bot] chat tries to beat randomised pokemon white before dougdoug streams again")
 
+    # sends dougdoug channel link
     @commands.command()
     async def dougdoug(self, ctx: commands.Context):
         await ctx.send("[bot] https://www.twitch.tv/dougdoug")
 
+    # ROGGED BUT FERN
     @commands.command()
     async def RIGGED(self, ctx: commands.Context):
         await ctx.send("[bot] BUT FAIR")
 
+    # sus
     @commands.command()
     async def areYouAnImposter(self, ctx: commands.Context):
         await ctx.send("no, no I am not. I am the original early gang, beep bop boop.")
 
+    # sends a list of all the bots
     @commands.command()
     async def bots(self, ctx: commands.Context):
         await ctx.send("[bot] input bot, aka chris snack, presses a random button every minute or so, idle bot steals your controller if you leave it alone for five minutes, and theres a 5% chance of your own input being completely random or the opposite")
 
+    # sends a list of commands
     @commands.command()
     async def menu(self, ctx: commands.Context):
-        await ctx.send("[bot] !what, !bots, !song, !controls, !vote, !poll, !discord, !watchtime, !bp, !donate")
+        await ctx.send("[bot] !what, !bots, !song, !controls, !vote, !poll, !discord, !watchtime, !bp, !donate, !playlist, !bpShop")
 
+    # sends link to discord
     @commands.command()
     async def discord(self, ctx: commands.Context):
         await ctx.send("[bot] https://discord.gg/eYSUuqNUvb")
 
+    # sends link to tiltify page
     @commands.command()
     async def donate(self, ctx: commands.Context):
         await ctx.send("[bot] https://tiltify.com/@early-gang/profile")
 
+    # sends link to stream music playlist
     @commands.command()
     async def playlist(self, ctx: commands.Context):
         await ctx.send("[bot] https://open.spotify.com/playlist/0GhV1AmrhugyYsCb8gEHsS?si=02930e0a7cd54a03")
+
+    # lets sna1l raid
+    @commands.command()
+    async def raid(self, ctx: commands.Context):
+        if ctx.author.name == "sna1l_boy":
+            ctx.message.content = ctx.message.content.replace("!raid ", "")
+            raid(yourChannelName, ctx.message.content)
+            openGame()
+
+    # lets sna1l mod people
+    @commands.command()
+    async def mod(self, ctx: commands.Context):
+        if ctx.author.name == "sna1l_boy":
+            ctx.message.content = ctx.message.content.replace("!mod ", "")
+            requests.post("https://api.twitch.tv/helix/moderation/moderators?broadcaster_id=" + getBroadcasterId(yourChannelName) + "&user_id=" + getBroadcasterId(ctx.message.content), headers={"Authorization": f"Bearer {accessToken}", "Client-Id": clientID})
+
+    # allows mods to ping sna1l if something goes wrong with the code
+    @commands.command()
+    async def panicCode(self, ctx: commands.Context):
+
+        # getting mod list
+        connected = False
+        while not connected:
+            try:
+                response = requests.get("https://api.twitch.tv/helix/users", headers={"Client-ID": clientID, "Authorization": f"Bearer {accessToken}"})
+                rateLimit = response.headers.get("Ratelimit-Remaining")
+                if rateLimit != "0":
+                    response = requests.get("https://api.twitch.tv/helix/moderation/moderators?broadcaster_id=" + getBroadcasterId(yourChannelName), headers={"Authorization": f"Bearer {accessToken}", "Client-Id": clientID})
+                    connected = True
+                else:
+                    sleep(5)
+            except:
+                sleep(5)
+        modIds = []
+        for mod in response.json().get("data"):
+            modIds += [mod.get("user_id")]
+
+        # updates and writes the variable in the config that tells the discord bot to send the messages
+        if getBroadcasterId(ctx.author.name) in modIds or ctx.author.name == yourChannelName:
+            with open(os.path.abspath((os.path.join(directory, "config.ini"))), "r") as file:
+                lines = file.readlines()
+            for i, line in enumerate(lines):
+                if line.startswith("ping kai ="):
+                    lines[i] = "ping kai = True\n"
+                    break
+            with open(os.path.abspath((os.path.join(directory, "config.ini"))), "w") as file:
+                file.writelines(lines)
+
+    # allows mods to ping fizz if something goes wrong with the stream
+    @commands.command()
+    async def panicStream(self, ctx: commands.Context):
+
+        # getting mod list
+        connected = False
+        while not connected:
+            try:
+                response = requests.get("https://api.twitch.tv/helix/users", headers={"Client-ID": clientID, "Authorization": f"Bearer {accessToken}"})
+                rateLimit = response.headers.get("Ratelimit-Remaining")
+                if rateLimit != "0":
+                    response = requests.get("https://api.twitch.tv/helix/moderation/moderators?broadcaster_id=" + getBroadcasterId(yourChannelName), headers={"Authorization": f"Bearer {accessToken}", "Client-Id": clientID})
+                    connected = True
+                else:
+                    sleep(5)
+            except:
+                sleep(5)
+        modIds = []
+        for mod in response.json().get("data"):
+            modIds += [mod.get("user_id")]
+
+        # updates and writes the variable in the config that tells the discord bot to send the messages
+        if getBroadcasterId(ctx.author.name) in modIds or ctx.author.name == yourChannelName:
+            with open(os.path.abspath((os.path.join(directory, "config.ini"))), "r") as file:
+                lines = file.readlines()
+            for i, line in enumerate(lines):
+                if line.startswith("ping fizz ="):
+                    lines[i] = "ping fizz = True\n"
+                    break
+            with open(os.path.abspath((os.path.join(directory, "config.ini"))), "w") as file:
+                file.writelines(lines)
 
     @commands.command()
     async def song(self, ctx: commands.Context):
@@ -75,7 +213,7 @@ class Bot(commands.Bot):
         if "access_token" in data:
             accessToken = data["access_token"]
         else:
-            print(f"error refreshing token: {data}")
+            print("error refreshing token: " + data)
             accessToken = None
 
         # getting current song
@@ -87,4 +225,4 @@ class Bot(commands.Bot):
             else:
                 await ctx.send("[bot] no song playing")
         else:
-            await ctx.send("[bot] cant get song")
+            await ctx.send("[bot] can't get song")
