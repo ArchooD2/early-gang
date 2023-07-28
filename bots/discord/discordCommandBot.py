b8106acfd49cdc0da9122e9711cf90b2c0cd17e3
sna1lBoy <jadenray1@proton.me>
2023-07-28 14:51:25 -0500

    added void role on reaction

--- bots/discord/discordCommandBot.py
+++ bots/discord/discordCommandBot.py
@@ -1,6 +1,12 @@
 # basic bot to host all the commands needed for the discord server
+# TODO reminders
+# TODO cah
+# TODO role menus
 
 # imports
+import asyncio
+import random
+
 import discord
 from libraries.autoStream import *
 
@@ -22,3 +28,16 @@ async def on_member_update(before, after):
     if discord.utils.get(after.roles, id=1132732881140203571) or discord.utils.get(after.roles, id=1125863416708472832):
         await bot.get_channel(1132769096900034700).send("Void Visitor has arrived: " + before.mention + ". Void Count: " + str(len(discord.utils.get(bot.get_guild(1122315794538303528).roles, id = 1132732881140203571).members) + len(discord.utils.get(bot.get_guild(1122315794538303528).roles, id = 1125863416708472832).members)))
 
+# voids user for a random amount of time on reaction to the role menu
+@bot.event
+async def on_raw_reaction_add(payload):
+    message = await bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+    if payload.message_id == 1132625274077454416 and discord.utils.get(message.reactions, emoji="🍿"):
+        await payload.member.add_roles(bot.guilds[0].get_role(1125863416708472832))
+        asyncio.create_task(voidWait(payload.member, discord.utils.get(message.reactions, emoji="🍿")))
+
+# waiting to unvoid
+async def voidWait(user, reaction):
+    await asyncio.sleep(random.randint(120, 10800))
+    await user.remove_roles(bot.guilds[0].get_role(1125863416708472832))
+    await reaction.remove(user)
