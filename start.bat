83f589aaf04b3e1e21e49e791cf6c941dda21057
sna1lBoy <jadenray1@proton.me>
2023-08-01 18:12:28 -0500

    removed ram bot

--- start.bat
+++ start.bat
@@ -5,7 +5,6 @@ start /B py bots/twitch/commandBot.py
 start /B py bots/twitch/econBot.py
 start /B py bots/twitch/pollBot.py
 start /B py bots/discord/sna1lBot.py
-start /B py bots/discord/ramcicleBot.py
 start /B py bots/discord/gwrbullBot.py
 start /B py bots/discord/fizzyghostBot.py
 start /B py bots/discord/actualbirdmanBot.py
